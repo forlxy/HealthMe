44ec40df099e3ca014cd71af9fb7b41da2bf7586
kasaluoqi <kasaluoqi@gmail.com>
2018-09-24 23:13:27 +1000

    v0.4

---
+++ AssignmentAgain/AssignmentAgain/Models/reservation.cs
@@ -0,0 +1,25 @@
+namespace AssignmentAgain.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("reservation")]
+    public partial class reservation
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id { get; set; }
+
+        public int location_id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string user_id { get; set; }
+
+        public virtual AspNetUser AspNetUser { get; set; }
+
+        public virtual location location { get; set; }
+    }
+}
